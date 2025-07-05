e68dfcdfe4c836f56376620e7f88a211f632561c
Adam Edison <adam.edison@gmail.com>
2025-07-05 09:53:46 -0400

    chore(netlify): Update Next.js configuration and package scripts
    - Added output and trailingSlash options to next.config.ts for improved static export handling.
    - Configured images to be unoptimized in next.config.ts.
    - Modified the static script in package.json to simplify the build process.
    - Made compatible with netlify deployment process as documented here:
    
    https://docs.netlify.com/frameworks/next-js/overview/

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true
+  reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
